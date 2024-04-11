aab5f26f0a1107f4a25e2adc95e69910df6e271c
Anri Kezeroti <AKezeroti@abacusnext.com>
2024-04-11 19:38:24 +0400

    Add Validation attributes and Suppress automatic 400 response and add Json serialization cycles ignore

--- Dtos/CreatePostDto.cs
+++ Dtos/CreatePostDto.cs
@@ -1,8 +1,13 @@
-﻿namespace Reddit.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Reddit.Dtos
 {
     public class CreatePostDto
     {
+        [Required]
+        [MinLength(3, ErrorMessage = "The title must be at least 3 characters long.")]
         public string Title { get; set; }
+        [Required]
         public string Content { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,12 +2,20 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Reddit;
 using Reddit.Mapper;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
+{
+    options.SuppressModelStateInvalidFilter = true; 
+}).AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
